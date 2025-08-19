c7388cb8eb49bbc5495324d7b76624f39464be6d
Joyce Gonzaga <gonzaga.joyce015@gmail.com>
2025-08-19 19:59:20 +0800

    changes

--- crowdfunding/fundraisers/models.py
+++ crowdfunding/fundraisers/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import get_user_model
+from django.contrib.auth import get_user_model
 
 class Fundraiser(models.Model):
     title = models.CharField(max_length=200)

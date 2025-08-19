fec614f62d2ba7ea73e5660479ad4cb2dc1b2db2
Joyce Gonzaga <gonzaga.joyce015@gmail.com>
2025-08-19 19:27:17 +0800

    user models and serializers

--- crowdfunding/fundraisers/models.py
+++ crowdfunding/fundraisers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import get_user_model
 
 class Fundraiser(models.Model):
     title = models.CharField(max_length=200)
@@ -7,7 +8,17 @@ class Fundraiser(models.Model):
     image = models.URLField()
     is_open = models.BooleanField()
     date_created = models.DateTimeField(auto_now_add=True)
-   
+    owner = models.ForeignKey(
+        get_user_model(),
+        related_name='owned_fundraisers',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.title
+
+    def total_pledged(self):
+        return self.pledges.aggregate(models.Sum('amount'))['amount__sum'] or 0
 
 class Pledge(models.Model):
     amount = models.IntegerField()
@@ -18,4 +29,10 @@ class Pledge(models.Model):
         related_name='pledges',
         on_delete=models.CASCADE
 
+    )
+    supporter = models.ForeignKey(
+        get_user_model(),
+        related_name='pledges',
+        on_delete=models.CASCADE
+    
     )

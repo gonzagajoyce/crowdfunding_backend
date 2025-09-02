6d1ebd850087d0c22b67cfdf1f40390515e38d7a
Joyce <Gonzaga.joyce015@gmail.com>
2025-09-02 19:11:53 +0800

    deployment settup

--- crowdfunding/fundraisers/urls.py
+++ crowdfunding/fundraisers/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('fundraisers/', views.FundraiserListView.as_view()),
     path('fundraisers/<int:pk>/', views.FundraiserDetailView.as_view()),
-    path('pledges/', views.PledgeListView.as_view()),   
+    path('pledges/', views.PledgeListView.as_view()),
+    path('pledges/<int:pk>/', views.PledgeDetailView.as_view()),   
 ]
--- crowdfunding/fundraisers/views.py
+++ crowdfunding/fundraisers/views.py
@@ -81,4 +81,36 @@ class PledgeListView(APIView):
                 serializer.errors, 
                 status=status.HTTP_400_BAD_REQUEST
                 )
+        
+class PledgeDetail(APIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsSupporterOrReadOnly]
+
+    def get_pledge(self,pk):
+        try:
+            pledge = Pledge.objects.get(pk=pk)
+            self.check_object_permissions(self.request, pledge)
+            return pledge
+        except Pledge.DoesNotExist:
+            raise Http404
+        
+    def get(self,request,pk):
+        pledge = self.get_pledge(pk)
+        serialized = PledgeSerializer(pledge)
+        return Response(serialized.data)
+    
+    def put(self,request,pk):
+        pledge = self.get_pledge(pk)
+        serializer = PledgeSerializer(
+            instance = pledge,
+            data=request.data,
+            partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
     
